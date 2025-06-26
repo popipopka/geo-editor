35182d043b23cbac3412b03fac6de3841d5d9b72
popipopka <eg.belykh@yandex.ru>
2025-06-27 00:32:17 +0300

    init

---
+++ src/main/java/ru/vistar/geoeditor/GeoEditorApplication.java
@@ -0,0 +1,13 @@
+package ru.vistar.geoeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoEditorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoEditorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vistar/geoeditor/GeoEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vistar.geoeditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoEditorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
